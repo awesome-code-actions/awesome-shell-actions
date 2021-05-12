3596782b5a844f1b3f4bec9f32f4d8d54fc62fc4
wucong <q1875486458@gmail.com>
2021-05-12 19:08:37 +0800

    update

--- scripts/ssh-action.sh
+++ scripts/ssh-action.sh
@@ -1,3 +1,15 @@
-connect-to-kuku() {
-    mosh cong@kuku
+to-kuku() {
+    mosh cong@172.29.175.252
+}
+
+to-kaka() {
+    mosh cong@172.29.179.128
+}
+
+to-wawa() {
+    mosh cong@172.29.50.236
+}
+
+to-k20() {
+    ssh u0_a296@172.29.171.175 -p 8022
 }
