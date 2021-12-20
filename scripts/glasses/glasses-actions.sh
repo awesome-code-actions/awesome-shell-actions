983da92e89784997caed41ef65cdca5748577298
congwu <congwu@alauda.io>
2021-12-21 00:01:50 +0800

    update

--- scripts/glasses/glasses-actions.sh
+++ scripts/glasses/glasses-actions.sh
@@ -2,3 +2,8 @@ how-many-systemcall-each-cpu() {
 	CFD="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 	sudo bpftrace $CFD/syscount-each-cpu.bt
 }
+
+list-symbol-by-file() {
+	local p=$1
+	objdump -tT $p
+}
---
+++ scripts/mc-actions.sh
@@ -0,0 +1,3 @@
+mc-open-hcml() {
+	java -jar ~/game/mc/HMCL-3.5.0.214.jar &!
+}
