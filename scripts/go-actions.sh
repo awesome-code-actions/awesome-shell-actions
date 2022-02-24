6b4e5e1ea5b46a0d683eae2fec441d83d3eb5a6b
cong <q1875486458@gmail.com>
2022-02-24 18:42:48 +0800

    temp

--- scripts/go-actions.sh
+++ scripts/go-actions.sh
@@ -9,3 +9,23 @@ go-use-china-proxy() {
 go-unset-go-proxy() {
     unset GOPROXY
 }
+
+go-list-test() {
+    local list=$(go test ./...  -list=. |grep -v '?')
+	export LIST=$list
+	local output=$( bash <<-EOF
+	python3 - <<-START
+		import os
+		list = os.environ["LIST"]
+		map = {}
+		for l in list.splitlines()
+			if l.startswith("ok")
+				map[]
+		print(list)
+	START
+	EOF
+	)
+    echo $output
+
+    return
+}
