2fe02f13b8e190b0312b08560bdadeb34dfde187
congwu <congwu@alauda.io>
2022-02-25 08:55:33 +0800

    add go-list-test

--- scripts/go-actions.sh
+++ scripts/go-actions.sh
@@ -9,3 +9,34 @@ go-use-china-proxy() {
 go-unset-go-proxy() {
     unset GOPROXY
 }
+
+
+go-run-one-test() {
+	local test=$(go-list-test | fzf)
+	local t=$(echo $test | cut -f1 -d' ')
+	local p=$(echo $test | cut -f2 -d' ')
+	echo "$t" "$p"
+	go test -v -run "$t" "$p"
+}
+
+go-list-test() {
+	local testlist=$(go test ./...  -list=. |grep -v '?')
+	exec 3<<< "$testlist"
+	python3  - <<-'START'
+	import sys
+	import os
+	data=os.read(3,10240).decode("utf-8")
+	m = {}
+	temp = []
+	for line in data.split('\n'):
+    	if line.startswith('ok'):
+        	package = line.split()[1]
+        	m[package] = temp
+        	temp = []
+        	continue
+    	temp.append(line)
+	for p, ts in m.items():
+    	for t in ts:
+        	print(t, p)
+	START
+}
