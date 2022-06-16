74ac7a08f153b18fbeaa6ea2dc5de8bd35b68a99
wucong <q1875486458@gmail.com>
2022-06-16 08:24:22 +0800

    update

--- scripts/glasses/systemtap.actions.sh
+++ scripts/glasses/systemtap.actions.sh
@@ -4,10 +4,10 @@ stap-hello-world() {
 	sudo stap -v -e 'probe vfs.read {printf("read performed\n"); exit()}'
 }
 
-default-or-select-docker-id()  {
+default-or-select-docker-id() {
 	local arg=$1
-	if [ -z "$arg" ] ; then
-		echo $(docker ps |tail -n+2 |fzf  |awk '{print $1}')
+	if [ -z "$arg" ]; then
+		echo $(docker ps | tail -n+2 | fzf | awk '{print $1}')
 		return
 	fi
 	echo $arg
@@ -19,12 +19,12 @@ stap-connect-docker() {
 	local base=$(zmx-find-base-of-action)
 	local pidns=$(docker-get-pidns-by-id $dockerid)
 	echo $pidns
-	sudo stap -B CONFIG_MODVERSIONS=y -g  -vv $base/connect_stat.stp $pidns
+	sudo stap -B CONFIG_MODVERSIONS=y -g -vv $base/connect_stat.stp $pidns
 }
 
-function list-all-syscall {
+function list-all-syscall() (
 	return
-}
+)
 
 function list-all-module {
 	return
