aca0eb06827e4f961fbd44863f488f7e8b310053
wucong <q1875486458@gmail.com>
2022-06-17 08:49:00 +0800

    update

--- scripts/kvm.actions.sh
+++ scripts/kvm.actions.sh
@@ -3,3 +3,11 @@
 function kvm-list-net() {
 	for vm in $(virsh list --state-running --name); do echo "$vm: " && virsh domiflist $vm | sed -n "3,$ { s,^,\t,; p }"; done
 }
+
+function kvm-list-host() {
+	virsh net-dhcp-leases default
+}
+
+function kvm-ssh() {
+	virsh net-dhcp-leases default  |grep ipv4 | awk '{print $5}'|cut -d '/' -f 1
+}
