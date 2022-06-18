0bf5332a3936bc4c173efed79fae73315775618d
wucong <q1875486458@gmail.com>
2022-06-18 11:44:44 +0800

    add some kvm action

--- scripts/kvm.actions.sh
+++ scripts/kvm.actions.sh
@@ -4,10 +4,17 @@ function kvm-list-net() {
 	for vm in $(virsh list --state-running --name); do echo "$vm: " && virsh domiflist $vm | sed -n "3,$ { s,^,\t,; p }"; done
 }
 
+function kvm-list-vm() {
+	virsh list --all
+}
+
 function kvm-list-host() {
 	virsh net-dhcp-leases default
 }
 
 function kvm-ssh() {
-	virsh net-dhcp-leases default  |grep ipv4 | awk '{print $5}'|cut -d '/' -f 1
+	local s=$(virsh net-dhcp-leases default  |grep ipv4 |awk '{print($5,$6)}'|fzf)
+	echo $s
+	local ip=$(echo $s|awk '{print $1}'|cut -d '/' -f 1)
+	ssh cong@$ip
 }
