0422084d9090050bfe887c3b0d598178435d7c0c
wucong <q1875486458@gmail.com>
2022-07-17 13:57:19 +0800

    update

---
+++ scripts/common.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+function time_now() {
+    echo $(date +%s%3N)
+}
+
+function time_format_time_diff() {
+    local start=$1
+    local end=$2
+    echo $(echo "scale=3; ($end-$start)/1000" | bc)s
+}
