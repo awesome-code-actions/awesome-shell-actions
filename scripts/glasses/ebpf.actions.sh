2909876198aad8b237adb48845ec06280049ddb7
cong <q1875486458@gmail.com>
2022-11-18 03:52:40 +0000

    Nov 18, 2022, 11:52 AM

--- scripts/glasses/ebpf.actions.sh
+++ scripts/glasses/ebpf.actions.sh
@@ -1,26 +1,30 @@
 #!/bin/bash
 
 function ebpf-hello-world() {
-    sudo bpftrace -v -e 'BEGIN { printf("hello world\n"); }'
+  sudo bpftrace -v -e 'BEGIN { printf("hello world\n"); }'
 }
 
 function ebpf-kprobe-hello() {
-    sudo bpftrace -e 'kprobe:ip_vs_reply4 {printf("%s\n",comm);}'
+  sudo bpftrace -e 'kprobe:ip_vs_reply4 {printf("%s\n",comm);}'
 }
 
 function how-many-systemcall-each-cpu() {
-    local base=$(dirname $(zmx-find-path-of-action))
-    echo $base
-    sudo bpftrace $base/syscount-each-cpu.bt
+  local base=$(dirname $(zmx-find-path-of-action))
+  echo $base
+  sudo bpftrace $base/syscount-each-cpu.bt
 }
 
 function list-symbol-by-file() {
-    local p=$1
-    objdump -tT $p
+  local p=$1
+  objdump -tT $p
 }
 
-function bpf-list-all-uprobe-of-file() {
-    local bin_path=$1
-    echo bin_path is $bin_path
-    sudo bpftrace -lv "uprobe:$bin_path:*" 2>&1
+function ebpf-list-all-uprobe-of-file() {
+  local bin_path=$1
+  echo bin_path is $bin_path
+  sudo bpftrace -lv "uprobe:$bin_path:*" 2>&1
+}
+
+function ebpf-ami-btf() {
+  cat /boot/config-$(uname -r) | grep BTF
 }
