0a6b332afa53e1cb3ac339a055324740e84376f7
cong <q1875486458@gmail.com>
2022-12-15 06:30:08 +0000

    Dec 15, 2022, 2:30 PM

--- scripts/docker-actions.sh
+++ scripts/docker-actions.sh
@@ -144,7 +144,6 @@ function docker-mems {
   numfmt --field=2 --from-unit=1024 --to=iec-i --suffix B </proc/meminfo | sed 's/ kB//'
   numfmt --field=2 --from-unit=1024 --to=iec-i --suffix B </sys/fs/cgroup/memory/memory.stat | sed 's/ kB//'
 }
-
 function docker-build-with-proxy {
   set -x
   if [ -n "$HTTP_PROXY" ]; then
