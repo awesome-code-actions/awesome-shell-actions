c8c9e7f7b5a9e6989809e276bcd21801ad559653
cong <q1875486458@gmail.com>
2023-06-08 09:41:18 +0800

    update

--- scripts/openssl.actions.sh
+++ scripts/openssl.actions.sh
@@ -7,6 +7,22 @@ function openssl-gen-cert() {
   openssl rsa -in example.key -out example-rsa.key
 }
 
+function openssl-gen-cert-expired() {
+  # Step 1: Save the current date
+  current_date=$(date -u)
+
+  # Step 2: Set the system date to a past date
+  sudo date --set="2000-01-01"
+
+  # Step 3: Create the certificate
+  openssl req -x509 -newkey rsa:4096 -sha256 -days 1 -nodes \
+    -keyout example.key -out example.crt -subj "/CN=example.com" \
+    -addext "subjectAltName=DNS:example.com,DNS:www.example.net,IP:10.0.0.1"
+  openssl rsa -in example.key -out example-rsa.key
+  # Step 4: Restore the system date
+  sudo date --set="$current_date"
+}
+
 function openssl-check-cert() {
   set -x
   local domain=$1
