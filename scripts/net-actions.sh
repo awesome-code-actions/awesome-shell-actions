e14d31496d84fd3653df78eec7953b120fc47d0d
cong <q1875486458@gmail.com>
2023-06-30 15:04:50 +0800

    gitsync auto-commit kaka

--- scripts/net-actions.sh
+++ scripts/net-actions.sh
@@ -77,7 +77,7 @@ function bridge-show() {
 function virt-bridge-if-show() {
   local ifname=$1
   local eth_type=$(ethtool -i $ifname | grep 'driver')
-  echo "  xss  eth_type $eth_type"
+  echo "$ifname eth_type $eth_type"
 }
 function bridge-if-show() {
   local ifname=$1
