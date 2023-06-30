126f5e9b48d3bfd693689830402ccc23c2615fda
cong <q1875486458@gmail.com>
2023-06-30 15:14:21 +0800

    gitsync auto-commit kaka

--- scripts/net-actions.sh
+++ scripts/net-actions.sh
@@ -78,7 +78,12 @@ function virt-bridge-if-show() {
   local ifname=$1
   local eth_type=$(ethtool -i $ifname | grep 'driver')
   echo "  $ifname eth_type $eth_type"
-  for dom in $(virsh list |grep runn | awk '{print $2}')
+  for dom in $(virsh list |grep runn | awk '{print $2}') ; do
+    if virsh domiflist $dom | grep -q $ifname; then
+      echo "   used in vm $dom"
+    fi
+    echo "   used in vm $dom mac"
+  done
 
 }
 function bridge-if-show() {
