eb7a0932bd91df6f03d0168d59a43192559c373b
cong <q1875486458@gmail.com>
2023-07-11 17:31:47 +0800

    gitsync auto-commit kaka

--- scripts/git-actions/git-actions.sh
+++ scripts/git-actions/git-actions.sh
@@ -1,167 +1,163 @@
 #!/bin/zsh
 
 function git-pull-current-remote-branch() {
-    git pull
+  git pull
 }
 
 function git-force-pull() {
-    current_branch=$(git branch --show-current | tr -d '\n\r')
-    git checkout -b "$current_branch"-bak-$(date '+%Y-%m-%d-%H-%M-%S')
-    git checkout master
-    git branch -D $current_branch
-    git checkout $current_branch
+  current_branch=$(git branch --show-current | tr -d '\n\r')
+  git checkout -b "$current_branch"-bak-$(date '+%Y-%m-%d-%H-%M-%S')
+  git checkout master
+  git branch -D $current_branch
+  git checkout $current_branch
 }
 
 # 检查当前的repo的帐号用户信息
 function git-check-current-info() {
-    echo "current info"
-    echo user is "=>"$(git config user.name)"<="
-    echo emil is "=>"$(git config user.email)"<="
+  echo "current info"
+  echo user is "=>"$(git config user.name)"<="
+  echo emil is "=>"$(git config user.email)"<="
 }
 
 function git-set-origin() {
-    # @arg-len: 1
-    url=$1
-    git remote set-url origin $url
+  # @arg-len: 1
+  url=$1
+  git remote set-url origin $url
 }
 
 function git-set-global-user() {
-    name=$1
-    git config --global user.name $name
+  name=$1
+  git config --global user.name $name
 }
 
 function git-set-global-email() {
-    email=$1
-    git config --global user.email $email
+  email=$1
+  git config --global user.email $email
 }
 
 # 查看当前commit的tag 如果当前commit没有tag 那么就是空
 function git-see-current-tag() {
-    git tag --points-at HEAD
+  git tag --points-at HEAD
 }
 
 # 查看包含特定comit的tag
 function git-which-tag-contains-this-commit() {
-    commit=$1
-    git tag --contains $commit
+  commit=$1
+  git tag --contains $commit
 }
 
 # 查看包含特定comit的分支
 function git-which-branch-contains-this-commit() {
-    commit=$1
-    git branch --contains $commit
+  commit=$1
+  git branch --contains $commit
 }
 
 # 查看包含某个特定文件 且文件内含有特定字符串的tag
 function git-which-tag-contains-this-file-and-text() {
-    file=$1
-    text=$2
-    ~/.zsh/awesome-shell-actions/scripts/git-actions/git-which-tag-contains-this-text.cr $file $text
+  file=$1
+  text=$2
+  ~/.zsh/awesome-shell-actions/scripts/git-actions/git-which-tag-contains-this-text.cr $file $text
 }
 
 function git-which-tag-contains-this-text() {
-    #!/bin/bash
+  # 获取当前存储库的分支列表
+  local branches=$(git branch --list | cut -c 3-)
+  # 遍历每个分支
+  for branch in $branches; do
+    # 切换到分支
+    git checkout $branch &>/dev/null
 
-# 获取当前存储库的分支列表
-branches=$(git branch --list | cut -c 3-)
-
-# 遍历每个分支
-for branch in $branches
-do
-  # 切换到分支
-  git checkout $branch &> /dev/null
-  
-  # 搜索字符串
-  echo "Searching in branch: $branch"
-  git grep <search-string>
-done
+    # 搜索字符串
+    echo "Searching in branch: $branch"
+    git grep $1
+  done
 
 }
 
 function git-commit-no-edit() {
-    git commit --amend --no-edit --allow-empty
+  git commit --amend --no-edit --allow-empty
 }
 
 function git-checkout-master() {
-    git checkout master
+  git checkout master
 }
 
 function git-contributor-by() {
-    user=$1
-    git log --author="$user" --pretty=tformat: --numstat | gawk '{ add += $1 ; subs += $2 ; loc += $1 - $2 } END { printf "added lines: %s removed lines : %s total lines: %s\n",add,subs,loc }'
+  user=$1
+  git log --author="$user" --pretty=tformat: --numstat | gawk '{ add += $1 ; subs += $2 ; loc += $1 - $2 } END { printf "added lines: %s removed lines : %s total lines: %s\n",add,subs,loc }'
 }
 
 function git-show-commit-by-user() {
-    user=$1
-    git log --oneline --author="$user"
+  user=$1
+  git log --oneline --author="$user"
 }
 
 function git-push-origin() {
-    # @tag: git-get-current_branch-name
-    current_branch=$(git branch --show-current | tr -d '\n\r')
-    git push origin $current_branch $@
+  # @tag: git-get-current_branch-name
+  current_branch=$(git branch --show-current | tr -d '\n\r')
+  git push origin $current_branch $@
 }
 
 function git-force-push-origin() {
-    current_branch=$(git branch --show-current | tr -d '\n\r')
-    git push origin $current_branch -f
+  current_branch=$(git branch --show-current | tr -d '\n\r')
+  git push origin $current_branch -f
 }
 
 function git-commit-no-edit-and-force-push-origin() {
-    git-commit-no-edit
-    git-force-push-origin
+  git-commit-no-edit
+  git-force-push-origin
 }
 
 function git-commit-no-edit-and-force-push-origin-no-verify() {
-    git-commit-no-edit
-    current_branch=$(git branch --show-current | tr -d '\n\r')
-    git push origin $current_branch -f --no-verify
+  git-commit-no-edit
+  current_branch=$(git branch --show-current | tr -d '\n\r')
+  git push origin $current_branch -f --no-verify
 }
 
 function git-add-modify-files-commit-update-and-push-origin() {
-    git add -u
-    git status
-    git commit -m "update"
-    git status
-    git push origin
+  git add -u
+  git status
+  git commit -m "update"
+  git status
+  git push origin
 }
 
 function git-add-modify-files-commit-no-edit-and-force-push-origin() {
-    git add -u
-    git status
-    git-commit-no-edit
-    git status
-    git-force-push-origin
+  git add -u
+  git status
+  git-commit-no-edit
+  git status
+  git-force-push-origin
 }
 
 function git-unset-http-proxy() {
-    git config --global --unset http.proxy
-    git config --global --unset https.proxy
+  git config --global --unset http.proxy
+  git config --global --unset https.proxy
 }
 
 function git-reset-all() {
-    zd
-    git reset --hard HEAD
-    git clean -fxd
+  zd
+  git reset --hard HEAD
+  git clean -fxd
 }
 
 function git-search-all-history() {
-    local msg=$1
-    git rev-list --all | (
-        while read revision; do
-            git grep -F "$msg" $revision
-        done
-    )
+  local msg=$1
+  git rev-list --all | (
+    while read revision; do
+      git grep -F "$msg" $revision
+    done
+  )
 }
 
 function git-sync() {
-    local p=$1
-    local base=$(dirname $(zmx-find-path-of-action))
-    echo $base
-    echo "--$p--"
-    $base/git-sync $p
+  local p=$1
+  local base=$(dirname $(zmx-find-path-of-action))
+  echo $base
+  echo "--$p--"
+  $base/git-sync $p
 }
 
 function git-default-proxy() {
-    git config --global http.proxy http://127.0.0.1:20172
+  git config --global http.proxy http://127.0.0.1:20172
 }
