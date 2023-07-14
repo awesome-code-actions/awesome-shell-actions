591b9883af1ebeaa270dd3f8a099a5a069e3ebe8
cong <q1875486458@gmail.com>
2023-07-14 18:33:37 +0800

    gitsync auto-commit kaka

--- scripts/github-actions.sh
+++ scripts/github-actions.sh
@@ -4,8 +4,14 @@ function git-clone-my-repo() {
 }
 
 function git-() {
-    local cfg=$(cat <<EOF
+  local cfg=$(
+    cat <<EOF
+Host github.com
+    Hostname ssh.github.com
+    Port 443
+    User git
 EOF
-)
+  )
+  echo $cfg
 
 }
