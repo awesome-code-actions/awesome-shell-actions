b036af7175110d1ea2047e99bc07a7def1c5c8d6
cong <q1875486458@gmail.com>
2024-03-12 12:09:14 +0800

    gitsync auto-commit kaka

--- scripts/gnome.actions.sh
+++ scripts/gnome.actions.sh
@@ -159,8 +159,7 @@ function test-get-input() {
 
 function gnome-list-workspace() (
   local ns=$(gsettings get org.gnome.desktop.wm.preferences workspace-names)
-  python -c <<<
-
+    local 
   local ns=$(echo "$ns" | sed 's/[^[:alnum:].-]//g')
   echo "$ns"
   IFS=" " read -r ns_list <<<"${ns:2:-2}"
