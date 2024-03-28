a7db745762c2ab212df8a67d1288e310d3a5499f
cong <q1875486458@gmail.com>
2024-03-28 21:00:13 +0800

    gitsync auto-commit kaka

--- scripts/glasses/systemtap.actions.sh
+++ scripts/glasses/systemtap.actions.sh
@@ -1,48 +1,49 @@
 #!/bin/bash
 
 function stap-hello-world() {
-    sudo stap -v -e 'probe vfs.read {printf("read performed\n"); exit()}'
+  sudo stap -v -e 'probe vfs.read {printf("read performed\n"); exit()}'
 }
 
 function default-or-select-docker-id() {
-    local arg=$1
-    if [ -z "$arg" ]; then
-        echo $(docker ps | tail -n+2 | fzf | awk '{print $1}')
-        return
-    fi
-    echo $arg
+  local arg=$1
+  if [ -z "$arg" ]; then
+    echo $(docker ps | tail -n+2 | fzf | awk '{print $1}')
     return
+  fi
+  echo $arg
+  return
 }
 
 function stap-connect-docker() {
-    local dockerid=$(default-or-select-docker-id $1)
-    local base=$(zmx-find-base-of-action)
-    local pidns=$(docker-get-pidns-by-id $dockerid)
-    echo $pidns
-    sudo stap -B CONFIG_MODVERSIONS=y -g -vv $base/connect_stat.stp $pidns
+  local dockerid=$(default-or-select-docker-id $1)
+  local base=$(zmx-find-base-of-action)
+  local pidns=$(docker-get-pidns-by-id $dockerid)
+  echo $pidns
+  sudo stap -B CONFIG_MODVERSIONS=y -g -vv $base/connect_stat.stp $pidns
 }
 
 function list-all-syscall() (
-    return
+  return
 )
 
 function list-all-module {
-    return
+  return
 }
 
 function list-all-vfs-file-operation {
-    return
+  return
 }
 
 function stap-list-all-kernel-function {
-    stap -L 'kernel.function("*")'
-    return
+  stap -L 'kernel.function("*")'
+  return
 }
 
-function list-all-module-function {
-    return
+function stap-list-all-module-function {
+  stap -L 'module("*").function("*")' 2>&1
+  return
 }
 
 function list-all-kernel-file {
-    return
+  return
 }
