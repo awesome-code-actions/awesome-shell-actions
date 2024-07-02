4a603f94e81f21c32695272a3bb4276fc8debb00
cong <q1875486458@gmail.com>
2024-07-03 00:18:26 +0800

    update

--- scripts/chrome-actions.sh
+++ scripts/chrome-actions.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
 function chrome-kill-me() {
-    ps -aux |grep chrome| awk '{print $2}' |xargs -I{} kill -9 {}  
+  ps -aux | grep chrome | awk '{print $2}' | xargs -I{} kill -9 {}
 }
+
+function chrome-get-debug-ws-ep() (
+  local ep=$(curl -s http://localhost:9222/json/version | jq -r '.webSocketDebuggerUrl')
+  if [ -n "$ep" ]; then
+    echo "$ep"
+    return
+  fi
+  /opt/google/chrome/chrome --remote-debugging-port=9222 &>~/.chrome.log &
+  sleep 3
+  local ep=$(curl -s http://localhost:9222/json/version | jq -r '.webSocketDebuggerUrl')
+  echo "$ep"
+)
