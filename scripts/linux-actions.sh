c7a71e1657401671f1dde232185a923329d3ea68
cong <q1875486458@gmail.com>
2024-08-04 14:08:08 +0800

    update

--- scripts/linux-actions.sh
+++ scripts/linux-actions.sh
@@ -1,29 +1,41 @@
 #!/bin/bash
 
 function openfile-get-limit() {
-    echo "current"
-    ulimit -n
-    echo "all"
-    ulimit -aH
-    ulimit -nH
-    ulimit -nS
+  echo "current"
+  ulimit -n
+  echo "all"
+  ulimit -aH
+  ulimit -nH
+  ulimit -nS
 }
 
 function openfile-set-limit() {
-    local lm=$1
-    ulimit -n $lm
+  local lm=$1
+  ulimit -n $lm
 }
 
 function edit-which() {
-    local w=$1
-    vim $(which $w)
+  local w=$1
+  vim $(which $w)
 }
 
 function ps-mem-human {
-    # while true;do sleep 1s ;(date;ps aux |grep alb | /root/cong/numfmt  --from-unit=1024 --to=iec --field 5,6   --padding 6) | tee -a ./mem.log;done
-    ps aux | numfmt --from-unit=1024 --to=iec --field 5,6 --padding 6
+  # while true;do sleep 1s ;(date;ps aux |grep alb | /root/cong/numfmt  --from-unit=1024 --to=iec --field 5,6   --padding 6) | tee -a ./mem.log;done
+  ps aux | numfmt --from-unit=1024 --to=iec --field 5,6 --padding 6
 }
 
 function has-config() {
-    grep $1 /boot/config-$(uname -r)
+  grep $1 /boot/config-$(uname -r)
+}
+
+function fuck-dns() {
+  sudo rm -rf /etc/resolve.conf
+  cat /etc/resolve.conf
+  local cfg=$(
+    cat <<EOF
+nameserver 127.0.0.1
+EOF
+  )
+  echo "$cfg" >/etc/resolve.conf
+  sudo chattr -i /etc/resolve.conf
 }
