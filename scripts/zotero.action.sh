45c203c04d748215835616d8bb0718544fcbaed9
cong <q1875486458@gmail.com>
2025-02-05 10:12:19 +0800

    update

--- scripts/zotero.action.sh
+++ scripts/zotero.action.sh
@@ -3,3 +3,7 @@
 function zotero-list-vh-note() (
   sqlite3 -json "file:///$HOME/Zotero/zotero.sqlite?immutable=1" "SELECT annote.text,annote.color,json_extract(annote.position,'$.value') as pos,json_extract(cache.data,'$.links.enclosure.title') as title from itemAnnotations as annote left join items on annote.parentItemID = items.itemID left join syncCache as cache on items.key = cache.key   where annote.color in ('#e56eee','#f19837')" | jq .
 )
+
+function zotero-init-markdown-link-export() (
+  return
+)
